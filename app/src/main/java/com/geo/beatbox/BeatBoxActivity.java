8d75b0fa95f1cd2b55860e866dd7c36e200ebfab
Alexey Tavgen <tavgen.alexey@gmail.com>
2017-05-30 16:47:51 +0300

    implement simple data binding

--- app/src/main/java/com/geo/beatbox/BeatBoxActivity.java
+++ app/src/main/java/com/geo/beatbox/BeatBoxActivity.java
@@ -1,13 +1,18 @@
 package com.geo.beatbox;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 
-public class BeatBoxActivity extends AppCompatActivity {
+public class BeatBoxActivity extends SingleFragmentActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_beat_box);
+        setContentView(R.layout.fragment_beat_box);
+    }
+
+    @Override
+    protected Fragment createFragment() {
+        return BeatBoxFragment.newInstance();
     }
 }
